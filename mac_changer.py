a278707e9ee990e81d1809b9afe86115b273415b
hugheylewis <103709866+hugheylewis@users.noreply.github.com>
2022-05-12 18:09:44 -0400

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+
+# This is a simple program to change your Linux device's MAC address.
+# While you can easily change your MAC address from the terminal,
+# this program incorporates error-checking to ensure the user is
+# entering the correct format, returning the current MAC address before
+# comitting the change to the specified interface, and provide verbosity
+# where applicable.
+# This program will soon incorporate a simple GUI to provide new Linux users
+# a simple and straightforward experience with early penetration testing techniques.
+# *** Please utilize the help menu with either -h or --help if you need information on
+# which flags to use in which order.
+
 import subprocess
 import optparse
 import re
